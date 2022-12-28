888b00d5ce99cf7157057325aee3293d3f7d19b6
Zahra Nejati <nejatizahra4@gmail.com>
2022-12-28 11:26:39 +0330

    bootstrap/react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import "./App.css";
+import { Button, Cart } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import useFetch from "./Hook/useFetch";
 
 function App() {
-  const { error, loading, data } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  return (
-    <div className="App">
-      <div>salam</div>
-      {error && <div>{error}</div>}
-      {loading && <div>loading</div>}
-      {data && (
-        <div>
-          {data.map((data) => {
-            return <div key={data.id}>{data.name}</div>;
-          })}
-        </div>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/Hook/useFetch.jsx
+++ src/Hook/useFetch.jsx
@@ -48,3 +48,21 @@ const useFetch = (url) => {
   return state;
 };
 export default useFetch;
+
+// const { error, loading, data } = useFetch(
+//   "https://jsonplaceholder.typicode.com/users"
+// );
+// return (
+//   <div className="App">
+//     <div>salam</div>
+//     {error && <div>{error}</div>}
+//     {loading && <div>loading</div>}
+//     {data && (
+//       <div>
+//         {data.map((data) => {
+//           return <div key={data.id}>{data.name}</div>;
+//         })}
+//       </div>
+//     )}
+//   </div>
+// );
